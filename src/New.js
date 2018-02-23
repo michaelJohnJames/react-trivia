b73a65cc56ddd47db770c5f895b9c07ab1820c7d
michaelJohnJames <32465022+michaelJohnJames@users.noreply.github.com>
2018-02-22 23:09:26 -0500

    parse api in new component

--- src/New.js
+++ src/New.js
@@ -6,24 +6,39 @@ export class New extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      categories: []
+      categories: [],
+      questions: [],
+      correctAnswer: [],
+      wrongAnswers: []
     }
   }
 
 
-
   componentWillMount() {
-    //const questions = [];
+    const questions = [];
+    const categories = [];
+    const answers = [];
+    const wrongAnswers = []
   fetch('https://opentdb.com/api.php?amount=10')
     .then(response => response.json())
     .then(data => {
-      let questions = data.results;
-      return questions.map((question) =>
-        console.log(question.category)
+      let info = data.results;
+      info.map((x) =>
+        //console.log(question.category)
+        //question.category;
+      { categories.push(x.category)
+        questions.push(x.question)
+        answers.push(x.correct_answer)
+        wrongAnswers.push(x.incorrect_answers)
+        }
       )
     })
+
       //data.response.map(data => data.results))
-    //this.setState({categories: categories});
+    this.setState({categories: categories});
+    this.setState({questions: questions});
+    this.setState({correctAnswer: answers })
+    this.setState({wrongAnswers: wrongAnswers})
     //console.log(this.state.categories)
   }
 
