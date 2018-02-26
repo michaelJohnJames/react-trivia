09e99b8f8274949c128e50e1fe74af2b1cfc6565
michaelJohnJames <32465022+michaelJohnJames@users.noreply.github.com>
2018-02-26 17:44:03 -0500

    clear interval of timer

--- src/Answer.js
+++ src/Answer.js
@@ -9,7 +9,7 @@ export class Answer extends React.Component {
     this.answersMapped = this.answersMapped.bind(this);
     this.checkAnswer = this.checkAnswer.bind(this);
     this.countDown = this.countDown.bind(this);
-    setTimeout(this.answersMapped, 3000)
+    setTimeout(this.answersMapped, 3000);
   }
 
 
@@ -21,22 +21,25 @@ export class Answer extends React.Component {
     ans.innerHTML += "<h4>" + this.props.correctAnswer[0] + "</h4>";
     for (let i = 0; i < array.length; i++) {
     ans.innerHTML += "<h4>" + array[i] + "</h4>"
+    }
+    this.countDown();
+  }
+
+
+countDown() {
+    const timer = document.getElementById('timer');
     var x = 10;
-    timer.innerHTML = x;
-
+    timer.textContent = x;
+     var t = setInterval(function() {
+     var x = timer.textContent;
+     timer.textContent = x - 1;
+      if (timer.textContent == 0) {
+        alert("Sorry, you are out of time!");
+        clearInterval(t);
+      }
+    }, 1000);
   }
-    var t = setInterval(this.countDown, 1000)
-}
 
-countDown(time) {
-  const timer = document.getElementById('timer');
-  var x = timer.textContent;
-  timer.innerHTML = x - 1;
-  if (timer.textContent == 0 ) {
-    console.log("Sorry, you've run out of time!");
-    clearInterval(setInterval(this.countDown, 1000))
-  }
-}
 
 
   checkAnswer(e) {
